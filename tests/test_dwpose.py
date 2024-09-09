a9c89345ba6c6d233a2f6c178cc48ad594d26cd9
badayvedat <badayvedat@gmail.com>
2024-09-09 20:58:39 +0000

    chore: lint

--- tests/test_dwpose.py
+++ tests/test_dwpose.py
@@ -8,15 +8,18 @@ import pytest
 def detector():
     return DWPoseDetector.from_pretrained()
 
+
 @pytest.fixture()
 def image():
     return load_image("tests/data/pose_sample.jpg")
+
 
 def test_output_type_pil(detector, image):
     from PIL import Image
 
     output = detector(image, output_type="pil")
     assert isinstance(output, Image.Image)
+
 
 def test_output_type_np(detector, image):
     import numpy as np
