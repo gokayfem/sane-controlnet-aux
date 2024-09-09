4aeac73f9e60e961ba508c63ecc7478ed613e8cc
badayvedat <badayvedat@gmail.com>
2024-09-09 21:21:41 +0000

    fix: add canny import

--- src/controlnet_aux/__init__.py
+++ src/controlnet_aux/__init__.py
@@ -5,4 +5,4 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from .dwpose import DWPoseDetector  # noqa
 from .canny import CannyDetector  # noqa
 
-__all__ = ["DWPoseDetector"]
+__all__ = ["DWPoseDetector", "CannyDetector"]
