243ed5e26f5b90ecb6bc008fce59664493f42bee
badayvedat <badayvedat@gmail.com>
2024-09-09 20:58:00 +0000

    refactor: tests

--- tests/test_dwpose.py
+++ tests/test_dwpose.py
@@ -4,23 +4,22 @@ from controlnet_aux.utils import load_image
 import pytest
 
 
-class TestDWPoseDetector:
-    @pytest.fixture(scope="class", autouse=True)
-    def detector(self):
-        return DWPoseDetector.from_pretrained()
+@pytest.fixture()
+def detector():
+    return DWPoseDetector.from_pretrained()
 
-    @pytest.fixture(scope="class", autouse=True)
-    def image(self):
-        return load_image("tests/data/pose_sample.jpg")
+@pytest.fixture()
+def image():
+    return load_image("tests/data/pose_sample.jpg")
 
-    def test_output_type_pil(self, detector, image):
-        from PIL import Image
+def test_output_type_pil(detector, image):
+    from PIL import Image
 
-        output = detector(image, output_type="pil")
-        assert isinstance(output, Image.Image)
+    output = detector(image, output_type="pil")
+    assert isinstance(output, Image.Image)
 
-    def test_output_type_np(self, detector, image):
-        import numpy as np
+def test_output_type_np(detector, image):
+    import numpy as np
 
-        output = detector(image, output_type="np")
-        assert isinstance(output, np.ndarray)
+    output = detector(image, output_type="np")
+    assert isinstance(output, np.ndarray)
